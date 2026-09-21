1d2ed2f432c9b596b951e0a6b1488ba4dc84c7cc
Your Name <you@example.com>
2026-09-21 21:15:24 +0000

    start app user + reddis session + resolve few docker file ussue

--- Services/User/srcs/index.php
+++ Services/User/srcs/index.php
@@ -1,3 +1,9 @@
 <?php
+$route = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 echo "<h1>CAMAGRU</h1>";
+echo "<p>$route</p>";
+echo "<h2>GET vars</h2>";
+echo "<pre>";
+print_r($_GET);
+echo "</pre>";
 ?>
